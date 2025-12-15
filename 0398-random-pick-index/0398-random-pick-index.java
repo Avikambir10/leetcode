40bcef1f63a0e03349e65d409fd10d0ff148f2f1
Avikam <123386663+Avikambir10@users.noreply.github.com>
2025-12-15 19:43:56 +0530

    Time: 953 ms (31.66%), Space: 61.8 MB (44%) - LeetHub

--- 0398-random-pick-index/0398-random-pick-index.java
+++ 0398-random-pick-index/0398-random-pick-index.java
@@ -1,22 +1,29 @@
 class Solution {
-    int[] nums;
+    private final int[] nums;
+    Random random;
 
     public Solution(int[] nums) {
         this.nums = nums;
+        random = new Random();
     }
 
     public int pick(int target) {
         int count = 0;
-        int ans = -1;
-
+        List<Integer> indices = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == target) {
-                count++;
-                if (Math.random() < 1.0 / count) {
-                    ans = i;
-                }
+                indices.add(i);
             }
         }
-        return ans;
+
+        int index = random.nextInt(indices.size());
+        return indices.get(index);
+
     }
 }
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int param_1 = obj.pick(target);
+ */
